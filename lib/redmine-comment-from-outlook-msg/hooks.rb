bd31d198215793f75977a5cc8d2594c197c08d1f
tom-mats <dawn.gray.sky@gmail.com>
2015-12-14 07:00:52 +0900

    refactoring

--- lib/redmine-comment-from-outlook-msg/hooks.rb
+++ lib/redmine-comment-from-outlook-msg/hooks.rb
@@ -4,30 +4,26 @@ module CommentFromeOutlookMsg
   def self.parse_mail(attachment)
     return nil
   end
-  class Hooks < Redmine::Hook::Listener
-    def controller_issues_new_before_save(context={})
-      if params.key? :attachments
-        if params[:attachments].is_a?(Array)
-          params[:attachments].each_char { |attachment|
-            mail_message = parse_mail(attachment)
-            if mail_message
-              params[:description] += "---\n" + mail_message
-            end
-          }
-        end
+  def self.add_message_from_mail(params, key)
+    additional_message = []
+    if params.key? :attachments
+      if params[:attachments].is_a?(Array)
+        params[:attachments].each { |attachment|
+          additional_message << parse_mail(attachment)
+        }
       end
     end
+    unless additional_message.empty?
+      params[key] = params[key].to_s + "---\n" + additional_message.join("---\n")
+    end
+  end
+
+  class Hooks < Redmine::Hook::Listener
+    def controller_issues_new_before_save(context={})
+      add_message_from_mail(params, :description)
+    end
     def controller_issues_edit_before_save(context={})
-      if params.key? :attachments
-        if params[:attachments].is_a?(Array)
-          params[:attachments].each_char { |attachment|
-            mail_message = parse_mail(attachment)
-            if mail_message
-              params[:notes] += "---\n" + mail_message
-            end
-          }
-        end
-      end
+      add_message_from_mail(params, :notes)
     end
   end
 end
